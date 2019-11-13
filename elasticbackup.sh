d4f0fd1f79c6db534e2e2881e05920134060e97b
Benoit JACQUOT <88.jacquot.benoit@gmail.com>
2019-11-13 14:08:25 +0100

    Adding Slack realtime notification support

--- elasticbackup.sh
+++ elasticbackup.sh
@@ -5,6 +5,8 @@
 # @author Orykami <88.jacquot.benoit@gmail.com>
 ##
 
+# Hostname
+HOST=${HOSTNAME}
 # Elasticbackup agent configuration path
 CONFIG_PATH="/etc/elasticbackup.conf"
 # Logger arguments
@@ -23,9 +25,14 @@ ES_REPO_TYPE="fs"
 ES_REPO_SETTINGS_JSON=""
 # Maximum snapshot to preserve (int)
 ES_SNAPSHOT_PRESERVE_COUNT=48
+# Webhook URL to notify for backup status
+SLACK_WEBHOOK_URL=""
 # Snapshot prefix
 ES_SNAPSHOT_PREFIX="elasticbackup"
-
+# Current run date
+RUN_DATE="$(date +%F_%H-%M)"
+# Timestamp when the script start to run
+START_TIME=`date +%s`
 ##
 # Retrieve configuration file from argument
 ##
@@ -52,7 +59,7 @@ else
 fi
 
 # Generation of next snapshot name
-NEXT_SNAPSHOT_NAME="${ES_SNAPSHOT_PREFIX}-$(date +%F_%H-%M)"
+NEXT_SNAPSHOT_NAME="${ES_SNAPSHOT_PREFIX}-${RUN_DATE}"
 
 ##
 # Write log to stdout/syslog
@@ -60,7 +67,21 @@ NEXT_SNAPSHOT_NAME="${ES_SNAPSHOT_PREFIX}-$(date +%F_%H-%M)"
 # @param $2 Log message
 ##
 log() {
-  logger -p $1 ${LOG_ARGS} $2
+  logger -p "$1" ${LOG_ARGS} "$2"
+  return 0
+}
+
+##
+# Notify slack via webhook with arguments
+# @param $1 Notification message
+#
+##
+notify_slack() {
+  # Notify #devops on Slack network if webhook is specified
+  if [[ -n ${SLACK_WEBHOOK_URL} ]]; then
+    printf -v JSON '{"text":"[%s][%s] %s"}' ${HOST} ${RUN_DATE} "$1"
+    ${CURL} -X POST -H 'Content-type: application/json' --data "$JSON" ${SLACK_WEBHOOK_URL} > /dev/null 2>&1
+  fi
   return 0
 }
 
@@ -71,19 +92,25 @@ log() {
 log user.info "Start elasticbackup.sh (Elasticsearch backup agent)"
 # Check if command `jq` is available
 if [[ -z ${JQ} ]]; then
-  log user.err "Command 'jq' not found, use sudo apt-get install jq first !"
+  ERROR_MESSAGE="Command 'jq' not found, use sudo apt-get install jq first !"
+  log user.err "${ERROR_MESSAGE}"
+  notify_slack "${ERROR_MESSAGE}"
   exit 1
 fi
 
 # Check if command `curl` is available
 if [[ -z ${CURL} ]]; then
-  log user.err "Command 'curl' not found, use sudo apt-get install curl first !"
+  ERROR_MESSAGE="Command 'curl' not found, use sudo apt-get install curl first !"
+  log user.err "${ERROR_MESSAGE}"
+  notify_slack "${ERROR_MESSAGE}"
   exit 1
 fi
 
 # Check if ES_REPO_NAME is defined
 if [[ -z ${ES_REPO_NAME} ]]; then
-  log user.err "Missing configuration ES_REPO_NAME"
+  ERROR_MESSAGE="Missing configuration ES_REPO_NAME"
+  log user.err "${ERROR_MESSAGE}"
+  notify_slack "${ERROR_MESSAGE}"
   exit 1
 fi
 
@@ -92,7 +119,9 @@ ES_STATE=$(${CURL} -s ${ES_URL}/_cluster/health | ${JQ} --raw-output .status)
 if [[ ${ES_STATE} == "green" ]] || [[ ${ES_STATE} == "yellow" ]]; then
   log user.info "Elasticsearch cluster ${ES_URL} is up (status : ${ES_STATE})"
 else
-  log user.err "Elasticsearch cluster ${ES_URL} seems down"
+  ERROR_MESSAGE="Elasticsearch cluster ${ES_URL} seems down"
+  log user.err "${ERROR_MESSAGE}"
+  notify_slack "${ERROR_MESSAGE}"
   exit 1;
 fi
 
@@ -102,12 +131,16 @@ if [[ "false" == ${ES_REPO_EXISTS} ]]; then
   log user.info "Repository ${ES_REPO_NAME} not found, create it from settings"
   # Check if ES_REPO_TYPE is defined
   if [[ -z ${ES_REPO_TYPE} ]]; then
-    log user.err "Missing configuration ES_REPO_TYPE for repository creation"
+    ERROR_MESSAGE="Missing configuration ES_REPO_TYPE for repository creation"
+    log user.err "${ERROR_MESSAGE}"
+    notify_slack "${ERROR_MESSAGE}"
     exit 1;
   fi
   # Check if ES_REPO_SETTINGS_JSON is defined
   if [[ -z ${ES_REPO_SETTINGS_JSON} ]]; then
-    log user.err "Missing configuration ES_REPO_SETTINGS_JSON for repository creation"
+    ERROR_MESSAGE="Missing configuration ES_REPO_SETTINGS_JSON for repository creation"
+    log user.err "${ERROR_MESSAGE}"
+    notify_slack "${ERROR_MESSAGE}"
     exit 1;
   fi
   # Create PUT request to ES cluster
@@ -115,7 +148,9 @@ if [[ "false" == ${ES_REPO_EXISTS} ]]; then
   if [[ $(echo ${RESPONSE} | jq --raw-output .acknowledged) == "true" ]]; then
     log user.info "Repository [${ES_REPO_TYPE}:${ES_REPO_NAME}] created on ES"
     else
-      log user.err "Repository [${ES_REPO_TYPE}:${ES_REPO_NAME}] creation failed : ${RESPONSE}"
+      ERROR_MESSAGE="Repository [${ES_REPO_TYPE}:${ES_REPO_NAME}] creation failed : ${RESPONSE}"
+      log user.err "${ERROR_MESSAGE}"
+      notify_slack "${ERROR_MESSAGE}"
       exit 1
     fi
 fi
@@ -134,7 +169,9 @@ RESPONSE=$(${CURL} -s -X PUT "${ES_URL}/_snapshot/${ES_REPO_NAME}/${NEXT_SNAPSHO
 if [[ "$(echo ${RESPONSE} | jq --raw-output .snapshot.state)" == "SUCCESS" ]]; then
   log user.info "Snapshot '${NEXT_SNAPSHOT_NAME}' created"
 else
-  log user.err "Failed to create snapshot '${NEXT_SNAPSHOT_NAME}' : ${RESPONSE}"
+  ERROR_MESSAGE="Failed to create snapshot '${NEXT_SNAPSHOT_NAME}' : ${RESPONSE}"
+  log user.err "${ERROR_MESSAGE}"
+  notify_slack "${ERROR_MESSAGE}"
   exit 1;
 fi
 
@@ -158,10 +195,22 @@ if [[ ${SNAPSHOT_COUNT} -gt ${ES_SNAPSHOT_PRESERVE_COUNT} ]]; then
       if [[ "$(echo ${RESPONSE} | jq --raw-output .acknowledged)" == "true" ]]; then
         log user.info "Snapshot ${OLD_SNAPSHOT} pruned"
       else
-        log user.warning "Failed to remove snapshot ${OLD_SNAPSHOT} : $(echo ${RESPONSE} | jq --raw-output .error)"
+        ERROR_MESSAGE="Failed to remove snapshot ${OLD_SNAPSHOT} : $(echo ${RESPONSE} | jq --raw-output .error)"
+        log user.err "${ERROR_MESSAGE}"
+        notify_slack "${ERROR_MESSAGE}"
       fi
     done
   fi
+fi
+
+# Create log entry for backup trace
+DURATION=$((`date +%s` - ${START_TIME}))
+log user.info "Backup completed in ${DURATION} seconds"
+
+# Notify #devops on Slack network if webhook is specified
+if [[ -n ${SLACK_WEBHOOK_URL} ]]; then
+  SUCCESS_MESSAGE="ES snapshot ${NEXT_SNAPSHOT_NAME} completed in ${DURATION} second(s)"
+  notify_slack "${SUCCESS_MESSAGE}"
 fi
 
 exit 0
